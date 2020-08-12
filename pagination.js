2db1b2b743c7d40681e890a4a6a2f0b8a0443515
AbhinavAdepu <abhinav.adepu@outlook.com>
2020-08-12 21:55:59 +0400

    Nisum Task
    Pls review

---
+++ pagination.js
@@ -0,0 +1,748 @@
+const list_itemsz = [
+  {
+    id: "Index",
+    name: "Name",
+    gender: "Gender",
+    age: "Age",
+    state: "State",
+    city: "City"
+  },
+  {
+    id: 0,
+    name: "Ramsey Cummings",
+    gender: "male",
+    age: 52,
+    state: "South Carolina",
+    city: "Glendale"
+  },
+  {
+    id: 1,
+    name: "Stefanie Huff",
+    gender: "female",
+    age: 70,
+    state: "Arizona",
+    city: "Beaverdale"
+  },
+  {
+    id: 2,
+    name: "Mabel David",
+    gender: "female",
+    age: 52,
+    state: "New Mexico",
+    city: "Grazierville"
+  },
+  {
+    id: 3,
+    name: "Frank Bradford",
+    gender: "male",
+    age: 61,
+    state: "Wisconsin",
+    city: "Saranap"
+  },
+  {
+    id: 4,
+    name: "Forbes Levine",
+    gender: "male",
+    age: 34,
+    state: "Vermont",
+    city: "Norris"
+  },
+  {
+    id: 5,
+    name: "Santiago Mcclain",
+    gender: "male",
+    age: 38,
+    state: "Montana",
+    city: "Bordelonville"
+  },
+  {
+    id: 6,
+    name: "Merritt Booker",
+    gender: "male",
+    age: 33,
+    state: "New Jersey",
+    city: "Aguila"
+  },
+  {
+    id: 7,
+    name: "Oconnor Wade",
+    gender: "male",
+    age: 18,
+    state: "Virginia",
+    city: "Kenmar"
+  },
+  {
+    id: 8,
+    name: "Leigh Beasley",
+    gender: "female",
+    age: 53,
+    state: "Texas",
+    city: "Alfarata"
+  },
+  {
+    id: 9,
+    name: "Johns Wood",
+    gender: "male",
+    age: 52,
+    state: "Maine",
+    city: "Witmer"
+  },
+  {
+    id: 10,
+    name: "Thompson Hays",
+    gender: "male",
+    age: 38,
+    state: "Nevada",
+    city: "Kipp"
+  },
+  {
+    id: 11,
+    name: "Hallie Mack",
+    gender: "female",
+    age: 19,
+    state: "Minnesota",
+    city: "Darrtown"
+  },
+  {
+    id: 12,
+    name: "Houston Santos",
+    gender: "male",
+    age: 24,
+    state: "Georgia",
+    city: "Crucible"
+  },
+  {
+    id: 13,
+    name: "Brandy Savage",
+    gender: "female",
+    age: 65,
+    state: "Idaho",
+    city: "Nord"
+  },
+  {
+    id: 14,
+    name: "Finch Barnett",
+    gender: "male",
+    age: 22,
+    state: "Ohio",
+    city: "Osmond"
+  },
+  {
+    id: 15,
+    name: "Nicole Crosby",
+    gender: "female",
+    age: 77,
+    state: "Kentucky",
+    city: "Fairfield"
+  },
+  {
+    id: 16,
+    name: "Carrie Mcconnell",
+    gender: "female",
+    age: 26,
+    state: "South Dakota",
+    city: "Waikele"
+  },
+  {
+    id: 17,
+    name: "Ann James",
+    gender: "female",
+    age: 37,
+    state: "North Dakota",
+    city: "Siglerville"
+  },
+  {
+    id: 18,
+    name: "Becky Sanford",
+    gender: "female",
+    age: 48,
+    state: "Massachusetts",
+    city: "Celeryville"
+  },
+  {
+    id: 19,
+    name: "Kathryn Rios",
+    gender: "female",
+    age: 39,
+    state: "Delaware",
+    city: "Kylertown"
+  },
+  {
+    id: 20,
+    name: "Dotson Vaughn",
+    gender: "male",
+    age: 68,
+    state: "Arkansas",
+    city: "Monument"
+  },
+  {
+    id: 21,
+    name: "Wright Kline",
+    gender: "male",
+    age: 41,
+    state: "Missouri",
+    city: "Bynum"
+  },
+  {
+    id: 22,
+    name: "Lula Morgan",
+    gender: "female",
+    age: 52,
+    state: "Oregon",
+    city: "Mapletown"
+  },
+  {
+    id: 23,
+    name: "Kay Mendez",
+    gender: "female",
+    age: 50,
+    state: "Michigan",
+    city: "Twilight"
+  },
+  {
+    id: 24,
+    name: "Mona Maddox",
+    gender: "female",
+    age: 35,
+    state: "Wyoming",
+    city: "Wilmington"
+  },
+  {
+    id: 25,
+    name: "Fulton Velez",
+    gender: "male",
+    age: 66,
+    state: "Colorado",
+    city: "Loretto"
+  },
+  {
+    id: 26,
+    name: "Ericka Craft",
+    gender: "female",
+    age: 80,
+    state: "Nebraska",
+    city: "Beaulieu"
+  },
+  {
+    id: 27,
+    name: "Richmond Rodriguez",
+    gender: "male",
+    age: 62,
+    state: "Rhode Island",
+    city: "Vallonia"
+  },
+  {
+    id: 28,
+    name: "Olsen Farmer",
+    gender: "male",
+    age: 45,
+    state: "Connecticut",
+    city: "Romeville"
+  },
+  {
+    id: 29,
+    name: "Sophie Austin",
+    gender: "female",
+    age: 59,
+    state: "New Hampshire",
+    city: "Gorst"
+  },
+  {
+    id: 30,
+    name: "Alta Olsen",
+    gender: "female",
+    age: 58,
+    state: "Florida",
+    city: "Drytown"
+  },
+  {
+    id: 25,
+    name: "Fulton Velez",
+    gender: "male",
+    age: 66,
+    state: "Colorado",
+    city: "Loretto"
+  },
+  {
+    id: 26,
+    name: "Ericka Craft",
+    gender: "female",
+    age: 80,
+    state: "Nebraska",
+    city: "Beaulieu"
+  },
+  {
+    id: 27,
+    name: "Richmond Rodriguez",
+    gender: "male",
+    age: 62,
+    state: "Rhode Island",
+    city: "Vallonia"
+  },
+  {
+    id: 28,
+    name: "Olsen Farmer",
+    gender: "male",
+    age: 45,
+    state: "Connecticut",
+    city: "Romeville"
+  },
+  {
+    id: 29,
+    name: "Sophie Austin",
+    gender: "female",
+    age: 59,
+    state: "New Hampshire",
+    city: "Gorst"
+  },
+  {
+    id: 30,
+    name: "Alta Olsen",
+    gender: "female",
+    age: 58,
+    state: "Florida",
+    city: "Drytown"
+  },
+  {
+    id: 25,
+    name: "Fulton Velez",
+    gender: "male",
+    age: 66,
+    state: "Colorado",
+    city: "Loretto"
+  },
+  {
+    id: 26,
+    name: "Ericka Craft",
+    gender: "female",
+    age: 80,
+    state: "Nebraska",
+    city: "Beaulieu"
+  },
+  {
+    id: 27,
+    name: "Richmond Rodriguez",
+    gender: "male",
+    age: 62,
+    state: "Rhode Island",
+    city: "Vallonia"
+  },
+  {
+    id: 28,
+    name: "Olsen Farmer",
+    gender: "male",
+    age: 45,
+    state: "Connecticut",
+    city: "Romeville"
+  },
+  {
+    id: 29,
+    name: "Sophie Austin",
+    gender: "female",
+    age: 59,
+    state: "New Hampshire",
+    city: "Gorst"
+  },
+  {
+    id: 30,
+    name: "Alta Olsen",
+    gender: "female",
+    age: 58,
+    state: "Florida",
+    city: "Drytown"
+  },
+  {
+    id: 7,
+    name: "Oconnor Wade",
+    gender: "male",
+    age: 18,
+    state: "Virginia",
+    city: "Kenmar"
+  },
+  {
+    id: 8,
+    name: "Leigh Beasley",
+    gender: "female",
+    age: 53,
+    state: "Texas",
+    city: "Alfarata"
+  },
+  {
+    id: 9,
+    name: "Johns Wood",
+    gender: "male",
+    age: 52,
+    state: "Maine",
+    city: "Witmer"
+  },
+  {
+    id: 10,
+    name: "Thompson Hays",
+    gender: "male",
+    age: 38,
+    state: "Nevada",
+    city: "Kipp"
+  },
+  {
+    id: 11,
+    name: "Hallie Mack",
+    gender: "female",
+    age: 19,
+    state: "Minnesota",
+    city: "Darrtown"
+  },
+  {
+    id: 12,
+    name: "Houston Santos",
+    gender: "male",
+    age: 24,
+    state: "Georgia",
+    city: "Crucible"
+  },
+  {
+    id: 13,
+    name: "Brandy Savage",
+    gender: "female",
+    age: 65,
+    state: "Idaho",
+    city: "Nord"
+  },
+  {
+    id: 14,
+    name: "Finch Barnett",
+    gender: "male",
+    age: 22,
+    state: "Ohio",
+    city: "Osmond"
+  },
+  {
+    id: 15,
+    name: "Nicole Crosby",
+    gender: "female",
+    age: 77,
+    state: "Kentucky",
+    city: "Fairfield"
+  },
+  {
+    id: 16,
+    name: "Carrie Mcconnell",
+    gender: "female",
+    age: 26,
+    state: "South Dakota",
+    city: "Waikele"
+  },
+  {
+    id: 7,
+    name: "Oconnor Wade",
+    gender: "male",
+    age: 18,
+    state: "Virginia",
+    city: "Kenmar"
+  },
+  {
+    id: 8,
+    name: "Leigh Beasley",
+    gender: "female",
+    age: 53,
+    state: "Texas",
+    city: "Alfarata"
+  },
+  {
+    id: 9,
+    name: "Johns Wood",
+    gender: "male",
+    age: 52,
+    state: "Maine",
+    city: "Witmer"
+  },
+  {
+    id: 10,
+    name: "Thompson Hays",
+    gender: "male",
+    age: 38,
+    state: "Nevada",
+    city: "Kipp"
+  },
+  {
+    id: 11,
+    name: "Hallie Mack",
+    gender: "female",
+    age: 19,
+    state: "Minnesota",
+    city: "Darrtown"
+  },
+  {
+    id: 12,
+    name: "Houston Santos",
+    gender: "male",
+    age: 24,
+    state: "Georgia",
+    city: "Crucible"
+  },
+  {
+    id: 13,
+    name: "Brandy Savage",
+    gender: "female",
+    age: 65,
+    state: "Idaho",
+    city: "Nord"
+  },
+  {
+    id: 14,
+    name: "Finch Barnett",
+    gender: "male",
+    age: 22,
+    state: "Ohio",
+    city: "Osmond"
+  },
+  {
+    id: 15,
+    name: "Nicole Crosby",
+    gender: "female",
+    age: 77,
+    state: "Kentucky",
+    city: "Fairfield"
+  },
+  {
+    id: 16,
+    name: "Carrie Mcconnell",
+    gender: "female",
+    age: 26,
+    state: "South Dakota",
+    city: "Waikele"
+  }
+];
+// PAGINATION MECHANICS
+var jspagination = {
+  // init() : initialize pagination
+  list_element: document.getElementById("accordion-alt"),
+  pagination_element: document.getElementById("pagination"),
+  rows_per_page: 5,
+  current_page: 0,
+  list_items: list_itemsz,
+  init: function() {
+    this.list_element.innerHTML = "";
+
+    let start = this.rows_per_page * jspagination.current_page;
+    let end = start + this.rows_per_page;
+    let paginatedItems = this.list_items.slice(start, end);
+
+    //Creates Pagination Items over lOOP
+    for (let i = 0; i < paginatedItems.length; i++) {
+      let item = paginatedItems[i];
+
+      let item_element = document.createElement("div");
+      let next_element = document.createElement("div");
+      item_element.classList.add("accordion-head");
+
+      let iconz = document.createElement("i");
+      iconz.classList.add("arrow");
+      item_element.append(iconz);
+      let divELem = document.createElement("div");
+      divELem.innerText = item.name;
+      item_element.appendChild(this.addTable(item));
+      // document.getElementById("global-table").appendChild(this.returnTH());
+      next_element.style.padding = "10px";
+      next_element.innerText = "Address: " + item.city;
+      next_element.classList.add("accordion-body");
+      jspagination.list_element.appendChild(item_element);
+      jspagination.list_element.appendChild(next_element);
+    }
+    jspagination.SetupPagination();
+  },
+  returnTH: function() {
+    var tableHead = document.createElement("THEAD");
+    Object.keys(this.list_items[0]).forEach(value => {
+      var th = document.createElement("TH");
+      tableHead.appendChild(document.createTextNode(value));
+      tableHead.appendChild(th);
+    });
+    return tableHead;
+  },
+  //Returns a tables with rows based on Json Reponse
+  addTable: function(obj) {
+    var table = document.createElement("TABLE");
+    table.border = "1";
+    var tableBody = document.createElement("TBODY");
+    table.appendChild(tableBody);
+
+    var tr = document.createElement("TR");
+    tableBody.appendChild(tr);
+    // converts obj to Values and loop over to create Table TR
+    Object.values(obj).map(value => {
+      var td = document.createElement("TD");
+      td.appendChild(
+        document.createTextNode(
+          typeof value === "string"
+            ? value.length > 10
+              ? value.substring(0, 8) + "..."
+              : value
+            : value
+        )
+      );
+      tr.appendChild(td);
+    });
+    return table;
+  },
+  //Button trigger for page go to
+  PaginationButton: function(page) {
+    //Creates a buttong to switch to number of next level of pages
+    let button = document.createElement("button");
+    button.innerText = page;
+
+    if (jspagination.current_page == page) button.classList.add("active");
+
+    button.addEventListener("click", function() {
+      jspagination.current_page = page;
+      jspagination.init();
+
+      let current_btn = document.querySelector(".pagenumbers button.active");
+      current_btn.classList.remove("active"); //Remove all active
+
+      button.classList.add("active"); //add respective active class
+      jspagination.SetupPagination();
+      jsaccordion.init("accordion-alt", true);
+    });
+
+    return button;
+  },
+  //Divides pages based on the rows available
+  SetupPagination: function() {
+    this.pagination_element.innerHTML = "";
+
+    let page_count = Math.ceil(
+      jspagination.list_items.length / jspagination.rows_per_page
+    );
+
+    for (let i = 0; i < page_count; i++) {
+      let btn = jspagination.PaginationButton(i);
+
+      this.pagination_element.appendChild(btn);
+    }
+
+    var arrObj = document.getElementById("pagination").children;
+    var arrBtn = Array.from(document.getElementById("pagination").children);
+    for (let i = 0; i < arrBtn.length; i++) {
+      if (i >= this.current_page && i < this.current_page + 5) {
+        arrObj[i].style.display = "inline-block"; //Show only active 5
+      }
+      if (
+        (i >= this.current_page + 5 && i < arrBtn.length) ||
+        (i >= 0 && i < this.current_page) //Hide other page button apart from 5 page button
+      ) {
+        arrObj[i].style.display = "none";
+      }
+    }
+  },
+  //Logic to Handle Next and PRev Button in Pagination
+  prevNextBtnHandlers: function() {
+    document.getElementById("first-page").addEventListener("click", function() {
+      jspagination.current_page = 0;
+      jspagination.init();
+      jspagination.SetupPagination();
+    });
+    document.getElementById("last-page").addEventListener("click", function() {
+      let page_count = Math.ceil(
+        jspagination.list_items.length / jspagination.rows_per_page
+      ); //returns the page count logic
+      jspagination.current_page = page_count - 1;
+
+      jspagination.init(); // initiates pagination
+      jspagination.SetupPagination();
+    });
+    document.getElementById("prev-page").addEventListener("click", function() {
+      if (jspagination.current_page >= 1) {
+        jspagination.current_page = jspagination.current_page - 1;
+
+        jspagination.init();
+        jspagination.SetupPagination();
+      }
+    });
+    document.getElementById("next-page").addEventListener("click", function() {
+      let page_count = Math.ceil(
+        jspagination.list_items.length / jspagination.rows_per_page
+      );
+      if (jspagination.current_page < page_count - 1) {
+        jspagination.current_page = jspagination.current_page + 1;
+
+        jspagination.init();
+        jspagination.SetupPagination();
+      }
+    });
+  }
+};
+
+jspagination.init();
+
+// ACCORDION MECHANICS
+var jsaccordion = {
+  init: function(target, single) {
+    // init() : initialize accordion
+    // PARAM target : ID of accordion wrapper
+    //       single : Only one Row will only at a time
+
+    var headers = document.querySelectorAll("#" + target + " .accordion-head");
+    if (headers.length > 0) {
+      // Single is false by default
+      if (typeof single != "boolean") {
+        single = false;
+      }
+
+      // Attach onclick event to headers
+      if (single) {
+        for (let i = 0; i < headers.length; i++) {
+          if (i !== 0)
+            headers[i].addEventListener("click", jsaccordion.sselect);
+        }
+      } else {
+        for (let i = 0; i < headers.length; i++) {
+          if (i !== 0) headers[i].addEventListener("click", jsaccordion.select);
+        }
+      }
+    }
+  },
+
+  select: function() {
+    // select() : fired when user clicks on a header
+
+    var contents = this.nextElementSibling;
+    contents.classList.toggle("open");
+  },
+
+  sselect: function() {
+    // sselect() : fired when user clicks on a header, single drawer open
+    // Expands selected accordian
+    contents = this.nextElementSibling;
+    var arrowChild = contents.previousSibling.children;
+    if (!contents.classList.contains("open")) {
+      // Close all first
+      var contentsz = this.parentElement.getElementsByClassName(
+        "accordion-body"
+      );
+      var arrowz = this.parentElement.getElementsByClassName("accordion-head");
+      if (contentsz.length > 0) {
+        for (var content of contentsz) {
+          content.classList.remove("open");
+        }
+      }
+      if (arrowz.length > 0) {
+        for (var arrow of arrowz) {
+          console.log(arrow.children[0]);
+          arrow.children[0].classList.remove("open");
+        }
+      }
+      //Opens Respective
+      contents.classList.add("open");
+      arrowChild[0].classList.add("open");
+    } else {
+      contents.classList.remove("open");
+      arrowChild[0].classList.remove("open");
+    }
+  }
+};
+
+// ON PAGE LOAD
+window.addEventListener("load", function() {
+  jsaccordion.init("accordion-alt", true);
+  jspagination.prevNextBtnHandlers();
+});
